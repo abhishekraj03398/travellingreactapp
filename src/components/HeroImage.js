6022e63acde0a1e8f9814a0a100112cf4aa6609d
abhishekraj03398 <abhishekraj03398@gmail.com>
2023-03-27 20:00:34 +0530

    changed update

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -7,7 +7,7 @@ const HeroImage = (props) => {
    <div className='hero-image'>
     <div className='heading'>
         <h1>{props.heading}</h1>
-        <p>{props.text}</p>       
+        <p>{props.text}</p>        
      </div>
    </div>
   );
